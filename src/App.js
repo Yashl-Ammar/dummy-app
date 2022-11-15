77fef0ba73aa7f0b4dde1fe38a9ddb243f95746c
Yashl Ammar <78467616+Yashl-Ammar@users.noreply.github.com>
2022-11-15 18:21:03 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is the great Yashl Ammar</h1>
+      <h1>I am deploying this app on github pages</h1>
+
     </div>
   );
 }
